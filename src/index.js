8828e0c4d01c0a92a7a18f647ed2eb8535396b9e
Kirill Dzhumanov <Djum83@mail.ru>
2023-01-18 22:11:26 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
   
+  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  function convert_hundreds(number) {
+    if (number > 99) {
+        return ones[Math.floor(number / 100)] + ' hundred ' + convert_tens(number % 100);
+    }
+    else {
+        return convert_tens(number);
+    }
+  }
+
+  function convert_tens(number) {
+    if (number < 10) return ones[number];
+    else if (number >= 10 && number < 20) return teens[number - 10];
+    else {
+        return tens[Math.floor(number / 10)] + ' ' + ones[number % 10];
+    }
+  }
+
+    if (number === 0) return 'zero';
+    else return convert_hundreds(number).trim();
 }
